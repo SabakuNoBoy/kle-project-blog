f080af3ec3423168785a508ceb769eaa07708ab8
KLE Developer <kle@example.com>
2026-03-10 09:15:03 +0300

    feat: add has_posts filter to user api

--- kle-project-backend-main/app/Http/Controllers/Api/UserController.php
+++ kle-project-backend-main/app/Http/Controllers/Api/UserController.php
@@ -8,8 +8,18 @@ use App\Models\User;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return \App\Http\Responses\ApiResponse::success(User::all());
+        $query = User::query();
+
+        if ($request->has('has_posts')) {
+            $query->whereHas('posts', function ($q) {
+                $q->where('is_approved', true);
+            });
+        }
+
+        return \App\Http\Responses\ApiResponse::success(
+            \App\Http\Resources\Api\UserResource::collection($query->get())
+        );
     }
 }
